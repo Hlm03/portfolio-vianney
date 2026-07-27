461e9373f577d229dd84fccaa88f583c908febc1
Hlm03 <105984206+Hlm03@users.noreply.github.com>
2026-07-27 05:54:33 +0100

    fix: correct sitemap URL

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://vianney-hadonou.vercel.app',
+      url: 'https://portfolio-vianney.vercel.app',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
