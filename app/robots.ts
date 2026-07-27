fd20cd2fc477010be60925916e17fb93fdd98ee6
Hlm03 <105984206+Hlm03@users.noreply.github.com>
2026-07-27 05:54:13 +0100

    fix: correct sitemap URL

--- app/robots.ts
+++ app/robots.ts
@@ -3,6 +3,6 @@ import { MetadataRoute } from 'next'
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: { userAgent: '*', allow: '/' },
-    sitemap: 'https://vianney-hadonou.vercel.app/sitemap.xml',
+    sitemap: 'https://portfolio-vianney.vercel.app/sitemap.xml',
   }
 }
